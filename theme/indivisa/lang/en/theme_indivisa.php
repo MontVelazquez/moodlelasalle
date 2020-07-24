0ba0e504fad7312bd94eef101e95d7284e9cc750
mont_velazquez <montserrat.velazquez.1988@gmail.com>
2020-07-24 12:53:40 -0500

    Renombrando clean a indivisa

---
+++ theme/indivisa/lang/en/theme_indivisa.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_indivisa
+ * @copyright 2020 Ade.Luis.Montse
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Theme indivisa is a child theme of Boost.';
+// The name of our plugin.
+$string['pluginname'] = 'Indivisa';
+// Background image for login page.
+$string['login-background'] = 'Login page background image';
+// Background image for login page.
+$string['login-background_desc'] = 'An image that will be stretched to fill the background of the login page.';
+// Preset files setting.
+$string['presetfiles'] = 'Additional theme preset files';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+// Preset setting.
+$string['preset'] = 'Theme preset';
+// Preset help text.
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+// Raw SCSS setting.
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'Raw initial SCSS';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Right';
